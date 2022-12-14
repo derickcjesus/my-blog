c46a3624507a1782d6bc35f93277b616ecc605db
derickcjesus <derickcjesus@gmail.com>
2022-12-14 20:36:00 -0300

    adicionando o componente Card na página home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,12 +1,18 @@
-import "./styles.css"
+import Card from "../../components/Card";
+
+import "./styles.css";
 
 function Home() {
-    return (
-        <div>
-            <h1 className="blog-name">BLOG</h1>
-            <img src="line.svg"/>
-        </div>
-    );
+  return (
+    <>
+      <div>
+        <h1 className="blog-name">BLOG</h1>
+        <img src="line.svg" />
+      </div>
+
+      <Card />
+    </>
+  );
 }
 
 export default Home;
