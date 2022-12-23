f08cde4f7e5423ba51bc3833b121dd9dbb44366f
derickcjesus <derickcjesus@gmail.com>
2022-12-23 11:23:49 -0300

    criando a página dos posts

--- src/pages/Post/index.jsx
+++ src/pages/Post/index.jsx
@@ -1,5 +1,28 @@
+import "./styles.css";
+
 function Post() {
-    return <h1>Post</h1>
+  return (
+    <article>
+      <main>
+        <h1>2021 Style Guide: The Biggest Fall Trends</h1>
+        <p>
+          You guys know how much I love mixing high and low-end – it’s the best
+          way to get the most bang for your buck while still elevating your
+          wardrobe. The same goes for handbags! And honestly they are probably
+          the best pieces to mix and match. I truly think the key to completing
+          a look is with a great bag and I found so many this year that I wanted
+          to share a round-up of my most worn handbags.
+        </p>
+      </main>
+      <footer>
+        <p>
+          Postado por <br />
+          NOME DO USUARIO
+        </p>
+        <p>3 dias atrás</p>
+      </footer>
+    </article>
+  );
 }
 
 export default Post;
